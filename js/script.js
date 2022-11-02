946e6e2eef27cf131cf66ae49f6d2a8d22adddbb
MicheleRai <rainonemichele1999@gmail.com>
2022-11-02 15:27:40 +0100

    base

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;*/
+
+const eleGrid = document.querySelector('.grid');
+
+for (let i = 0; i < 100; i++) {
+
+	const eleCell = document.createElement('div');
+	eleCell.classList.add('cell');
+	eleGrid.append(eleCell);
+
+	eleCell.addEventListener('click', function () {
+		this.classList.toggle('active')
+	});
+}
